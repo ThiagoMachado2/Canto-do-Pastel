89f719ad2533885a1592844e1d35532e34ee79e3
ThiagoMachado2 <136006071+ThiagoMachado2@users.noreply.github.com>
2023-11-25 01:48:46 -0300

    Update Canto-do-Pastel
    Finalização a criação das tabelas e as 6 consultas pedidas.
    
    Co-Authored-By: Samuel <136186613+Sjr0405@users.noreply.github.com>
    Co-Authored-By: Jemima Santos <62177497+Jemima-Santos@users.noreply.github.com>

--- Canto-dos-Pasteis.sql
+++ Canto-dos-Pasteis.sql
@@ -2,198 +2,338 @@ CREATE DATABASE Canto_dos_Pasteis;
 
 USE Canto_dos_Pasteis;
 
-CREATE TABLE Clientes (
+CREATE TABLE clientes (
     cliente_id INT PRIMARY KEY AUTO_INCREMENT,
     nome_completo VARCHAR(100),
     nome_chamado VARCHAR(50),
     cpf VARCHAR(14) UNIQUE,
     data_nascimento DATE,
     telefone VARCHAR(15),
-    email VARCHAR(100),
-    endereco_id INT,
-    CONSTRAINT fk_endereco_cliente FOREIGN KEY (endereco_id) REFERENCES Enderecos(endereco_id)
+    email VARCHAR(100)
 );
 
-CREATE TABLE Enderecos (
+CREATE TABLE enderecos (
     endereco_id INT PRIMARY KEY AUTO_INCREMENT,
+    cliente_id INT,
     bairro VARCHAR(50),
+    numero VARCHAR(50),
     rua VARCHAR(50),
     cidade VARCHAR(50),
-    estado VARCHAR(2)
+    estado VARCHAR(2),
+    CONSTRAINT fk_cliente_endereco FOREIGN KEY (cliente_id) REFERENCES clientes(cliente_id)
 );
 
-CREATE TABLE Bebidas (
-    bebida_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE categorias (
+    categoria_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50)
+);
+
+CREATE TABLE produtos (   -- Pasteis, Bebidas e outros produtos
+    produto_id INT PRIMARY KEY AUTO_INCREMENT,
+    categoria_id INT,
     nome VARCHAR(50),
-    preco DECIMAL(10, 2)
+    CONSTRAINT fk_categoria_produto FOREIGN KEY (categoria_id) REFERENCES categorias(categoria_id)
 );
 
-CREATE TABLE Pasteis (
-    pastel_id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50),
-    preco DECIMAL(10, 2),
-    tamanho VARCHAR(10),
-    categoria VARCHAR(20) -- Ex: Vegano, Vegetariano, Sem Lactose
+CREATE TABLE tamanhos (  -- tamanho dos pasteis, bebidas e outros produtos
+    tamanho_id INT PRIMARY KEY AUTO_INCREMENT,
+    tamanho VARCHAR(100)
 );
 
-CREATE TABLE Recheios (
+CREATE TABLE tamanhosProdutos (   
+    tamanhopasteis_id INT PRIMARY KEY AUTO_INCREMENT,
+    produto_id INT,
+    tamanho_id INT,
+    preco DECIMAL(10, 2), -- onde vai ser definido o preço pelo o tamanho dos produtos
+    CONSTRAINT fk_produto_tamanhoproduto FOREIGN KEY (produto_id) REFERENCES produtos(produto_id),
+    CONSTRAINT fk_tamanho_tamanhoproduto FOREIGN KEY (tamanho_id) REFERENCES tamanhos(tamanho_id)
+);
+
+CREATE TABLE recheios (
     recheio_id INT PRIMARY KEY AUTO_INCREMENT,
-    pastel_id INT,
-    nome VARCHAR(50),
-    CONSTRAINT fk_pasteis FOREIGN KEY (pastel_id) REFERENCES Pasteis(pastel_id)
+    recheio VARCHAR(50)
 );
 
-CREATE TABLE Pedidos (
+CREATE TABLE recheiosProdutos(
+    recheiopasteis_id INT PRIMARY KEY AUTO_INCREMENT,
+    produto_id INT,
+    recheio_id INT,
+    CONSTRAINT fk_produto_recheiosproduto FOREIGN KEY (produto_id ) REFERENCES produtos(produto_id ),
+    CONSTRAINT fk_recheio_recheiosproduto FOREIGN KEY (recheio_id) REFERENCES recheios(recheio_id)
+);
+
+CREATE TABLE pedidos (
     pedido_id INT PRIMARY KEY AUTO_INCREMENT,
     cliente_id INT,
-    data_pedido DATE,
-    forma_pagamento VARCHAR(20),
-    CONSTRAINT fk_clientes FOREIGN KEY (cliente_id) REFERENCES Clientes(cliente_id)
+    data_pedido DATETIME DEFAULT NOW(),
+    forma_pagamento ENUM('D','PIX','CC','CD') NOT NULL DEFAULT 'D',
+    CONSTRAINT fk_clientes FOREIGN KEY (cliente_id) REFERENCES clientes(cliente_id)
 );
 
-CREATE TABLE ItensPedido (
+CREATE TABLE itensPedidos (
     item_id INT PRIMARY KEY AUTO_INCREMENT,
     pedido_id INT,
-    pastel_id INT,
+    produto_id INT,
     quantidade INT,
-    bebida_id INT,
-    CONSTRAINT fk_bebidas_pedido FOREIGN KEY (bebida_id) REFERENCES Bebidas(bebida_id),
-    CONSTRAINT fk_pedidos FOREIGN KEY (pedido_id) REFERENCES Pedidos(pedido_id),
-    CONSTRAINT fk_pasteis_pedido FOREIGN KEY (pastel_id) REFERENCES Pasteis(pastel_id)
+    CONSTRAINT fk_pedidos FOREIGN KEY (pedido_id) REFERENCES pedidos(pedido_id),
+    CONSTRAINT fk_produto_itenspedidos FOREIGN KEY (produto_id) REFERENCES produtos(produto_id) 
 );
 
-INSERT INTO Enderecos (bairro, cidade, estado) VALUES
-('Centro', 'Cidade A', 'SP'),
-('Bairro1', 'Cidade B', 'RJ'),
-('Bairro2', 'Cidade C', 'MG'),
-('Bairro3', 'Cidade D', 'RS'),
-('Bairro4', 'Cidade E', 'SC'),
-('Bairro5', 'Cidade F', 'PR'),
-('Bairro6', 'Rua X', 'Cidade G', 'SP'),
-('Bairro7', 'Rua Y', 'Cidade H', 'RJ'),
-('Bairro8', 'Rua Z', 'Cidade I', 'MG');
+-- Inserir dados na tabela 'clientes'
+INSERT INTO clientes (nome_completo, nome_chamado, cpf, data_nascimento, telefone, email)
+VALUES
+	('João Silva', 'João', '123.456.789-01', '1990-05-15', '(11) 98765-4321', 'joao.silva@email.com'),
+	('Maria Oliveira', 'Maria', '987.654.321-09', '1985-12-10', '(21) 99876-5432', 'maria.oliveira@email.com'),
+	('Carlos Santos', 'Carlos', '345.678.901-23', '1998-08-22', '(31) 91234-5678', 'carlos.santos@email.com'),
+    ('Fernanda Pereira', 'Fernanda', '567.890.123-45', '1995-02-28', '(41) 87654-3210', 'fernanda.pereira@email.com'),
+    ('Ricardo Oliveira', 'Ricardo', '789.012.345-67', '1980-11-05', '(51) 76543-2109', 'ricardo.oliveira@email.com');
+    
+    SELECT * FROM clientes;
 
-SELECT * FROM Enderecos;
+-- Inserir dados na tabela 'enderecos'
+INSERT INTO enderecos (cliente_id, bairro, numero, rua, cidade, estado)
+VALUES 
+    (1, 'Centro', '123', 'Rua A', 'São Paulo', 'SP'),
+    (2, 'Vila Madalena', '456', 'Rua B', 'São Paulo', 'SP'),
+    (3, 'Itaim Bibi', '789', 'Rua C', 'São Paulo', 'SP'),
+    (4, 'Jardins', '101', 'Avenida X', 'São Paulo', 'SP'),
+    (5, 'Boa Viagem', '567', 'Rua Y', 'Recife', 'PE');
+    
+    SELECT * FROM enderecos;
 
-INSERT INTO Clientes (nome_completo, nome_chamado, cpf, data_nascimento, telefone, email, endereco_id) VALUES
-('João Silva', 'João', '123.456.789-01', '1990-01-15', '(11) 98765-4321', 'joao@email.com', 1),
-('Maria Oliveira', 'Maria', '987.654.321-02', '1985-05-20', '(21) 98765-1234', 'maria@email.com', 2),
-('Carlos Pereira', 'Carlos', '111.222.333-44', '1988-11-10', '(31) 99999-8888', 'carlos@email.com', 3),
-('Ana Pereira', 'Ana', '555.666.777-88', '1980-06-25', '(41) 98765-4321', 'ana@email.com', 4),
-('Lucas Oliveira', 'Lucas', '999.888.777-66', '1995-12-10', '(48) 99999-1234', 'lucas@email.com', 5),
-('Mariana Santos', 'Mariana', '444.333.222-11', '1982-03-18', '(51) 98765-9876', 'mariana@email.com', 6),
-('Fernanda Souza', 'Fernanda', '777.888.999-10', '1983-08-22', '(11) 98765-4321', 'fernanda@email.com', 7),
-('Gabriel Lima', 'Gabriel', '666.555.444-33', '1992-05-15', '(21) 98765-1234', 'gabriel@email.com', 8),
-('Amanda Oliveira', 'Amanda', '222.333.444-55', '1980-12-10', '(31) 99999-8888', 'amanda@email.com', 9);
-
-SELECT * FROM Clientes;
-
-
-INSERT INTO Pasteis (nome, preco, tamanho, categoria) VALUES
-('Pastel Vegano', 8.50, 'Médio', 'Vegano'),
-('Pastel de Queijo', 5.00, 'Pequeno', 'Normal'),
-('Pastel de Bacon', 6.00, 'Grande', 'Normal'),
-('Pastel de Frango', 7.00, 'Médio', 'Normal'),
-('Pastel de Calabresa', 6.50, 'Grande', 'Normal'),
-('Pastel de Palmito', 7.50, 'Médio', 'Vegetariano'),
-('Pastel de Espinafre', 8.00, 'Grande', 'Vegetariano'),
-('Pastel de Berinjela', 7.00, 'Pequeno', 'Vegetariano'),
-('Pastel de Abóbora', 6.50, 'Médio', 'Vegano'),
-('Pastel de Lentilha', 7.00, 'Grande', 'Vegano'),
-('Pastel de Grão-de-bico', 6.50, 'Pequeno', 'Vegano');
-
-SELECT * FROM Pasteis;
-
-INSERT INTO Recheios (pastel_id, nome) VALUES
-(1, 'Recheio Vegano'),
-(2, 'Queijo'),
-(3, 'Bacon'),
-(4, 'Frango'),
-(5, 'Calabresa'),
-(6, 'Chocolate'),
-(7, 'Milho'),
-(8, 'Palmito'),
-(9, 'Espinafre'),
-(10, 'Berinjela'),
-(11, 'Abóbora'),
-(12, 'Lentilha'),
-(13, 'Grão-de-bico');
-
-SELECT * FROM Recheios;
-
-INSERT INTO Pedidos (cliente_id, data_pedido, forma_pagamento) VALUES
-(1, '2023-01-05', 'Cartão Credito'),
-(2, '2023-02-15', 'Dinheiro'),
-(3, '2023-03-20', 'Cartão Debito'),
-(4, '2023-04-10', 'Cartão Debito'),
-(5, '2023-05-20', 'Dinheiro'),
-(6, '2023-06-15', 'Cartão Credito'),
-(7, '2023-07-05', 'Dinheiro'),
-(8, '2023-08-12', 'Cartão Credito'),
-(9, '2023-09-18', 'Dinheiro');
-
-SELECT * FROM Pedidos;
-
-INSERT INTO Bebidas (nome, preco) VALUES
-('Suco de Laranja', 4.50),
-('Refrigerante', 3.00),
-('Água Mineral', 2.00);
-
-SELECT * FROM Bebidas;
-
-INSERT INTO ItensPedido (pedido_id, pastel_id, bebida_id, quantidade) VALUES
-(1, 1, 2, 2),
-(2, 2, 1, 1),
-(3, 3, 3, 3),
-(4, 4, 1, 2),
-(5, 5, 2, 1),
-(6, 6, 3, 3),
-(7, 7, NULL, 2),
-(8, 8, NULL, 1),
-(9, 9, NULL, 3);
-
-SELECT * FROM ItensPedido;
+-- Inserir dados na tabela 'categorias'
+INSERT INTO categorias (nome)
+VALUES 
+    ('Normal'),
+	('Vegano'),
+	('Vegetariano'),
+	('Sem Lactose'),
+    ('Bebidas'),
+    ('Personalizado'),
+    ('Outros Produtos');
+    
+    SELECT * FROM categorias;
 
 
+-- Inserir dados na tabela 'produtos'
+INSERT INTO produtos (categoria_id, nome)
+VALUES 
+    (1, 'Pastel de Carne'),
+	(2, 'Pastel Vegano de Espinafre'),
+	(3, 'Pastel Vegetariano de Palmito'),
+	(4, 'Pastel Sem Lactose de Frango'),
+	(5, 'Refrigerante'),
+	(5, 'Suco'),
+	(5, 'Água'),
+    (1, 'Pastel de Queijo'),
+    (2, 'Pastel Vegano de Couve-flor'),
+    (3, 'Pastel Vegetariano de Abobrinha'),
+    (4, 'Pastel Sem Lactose de Cogumelos'),
+	(1, 'Pastel de Frango'),
+    (1, 'Pastel de Queijo com Presunto'),
+    (2, 'Pastel Vegano de Tofu'),
+    (3, 'Pastel Vegetariano de Abobrinha com Ricota'),
+	(1, 'Pastel Personalizado');
+    
+    SELECT * FROM produtos;
+    
+-- Inserir dados na tabela 'tamanhos'
+INSERT INTO tamanhos (tamanho)
+VALUES 
+    ('Pequeno'),
+    ('Médio'),
+    ('Grande');
 
--- consulta 1: listar nomes de pastéis veganos vendidos para pessoas com mais de 18 anos
-SELECT P.nome
-FROM Pasteis P
-JOIN Recheios R ON P.pastel_id = R.pastel_id
-JOIN ItensPedido IP ON P.pastel_id = IP.pastel_id
-JOIN Pedidos PE ON IP.pedido_id = PE.pedido_id
-JOIN Clientes C ON PE.cliente_id = C.cliente_id
-WHERE P.categoria = 'Vegano' AND DATEDIFF(CURDATE(), C.data_nascimento) > 6570; -- 6570 dias ≈ 18 anos
+    SELECT * FROM tamanhos;
 
--- consulta 2: listar clientes com maior número de pedidos realizados em 1 ano agrupados por mês
-SELECT DATE_FORMAT(PE.data_pedido, '%m-%Y') AS mes_ano, C.nome_completo, COUNT(PE.pedido_id) AS num_pedidos
-FROM Pedidos PE
-JOIN Clientes C ON PE.cliente_id = C.cliente_id
-WHERE YEAR(PE.data_pedido) = YEAR(CURDATE())
-GROUP BY mes_ano, C.nome_completo
-ORDER BY num_pedidos DESC;
 
--- consulta 3: listar todos os pastéis que possuem bacon e queijo em seu recheio
-SELECT P.nome
-FROM Pasteis P
-JOIN Recheios R ON P.pastel_id = R.pastel_id
-WHERE R.nome = 'Bacon' AND R.nome = 'Queijo';
+-- Inserir dados na tabela 'tamanhosProdutos'
+INSERT INTO tamanhosProdutos (produto_id, tamanho_id, preco)
+VALUES 
+	(1, 1, 5.00),
+	(2, 2, 6.50),
+    (3, 3, 7.00),
+    (4, 2, 6.00),
+	(5, 1, 4.00), -- Refrigerante, tamanho pequeno
+	(5, 2, 6.00), -- Refrigerante, tamanho médio
+	(5, 3, 8.00), -- Refrigerante, tamanho grande
+	(6, 1, 5.00), -- Suco, tamanho pequeno
+	(6, 2, 7.00), -- Suco, tamanho médio
+	(6, 3, 9.00), -- Suco, tamanho grande
+	(7, 1, 2.00), -- Água, tamanho pequeno
+	(7, 2, 3.00), -- Água, tamanho médio
+	(7, 3, 4.00), -- Água, tamanho grande
+	(8, 1, 6.00),
+	(9, 2, 7.50),
+    (10, 3, 8.00),
+	(11, 1, 6.50),
+	(12, 2, 8.00),
+    (13, 3, 8.50),
+    (14, 2, 7.00),
+	(15, 1, 5.00),
+	(15, 2, 7.00),
+    (15, 3, 9.00);
+    
+    SELECT * FROM tamanhosProdutos;
 
--- consulta 4: mostrar o valor de venda total de todos os pastéis cadastrados no sistema
-SELECT SUM(P.preco) AS valor_total
-FROM Pasteis P;
 
--- consulta 5: listar todos os pedidos onde há pelo menos um pastel e uma bebida
-SELECT DISTINCT PE.pedido_id
-FROM Pedidos PE
-JOIN ItensPedido IP ON PE.pedido_id = IP.pedido_id
-LEFT JOIN Pasteis P ON IP.pastel_id = P.pastel_id
-LEFT JOIN Bebidas B ON IP.bebida_id = B.bebida_id
-WHERE P.pastel_id IS NOT NULL OR B.bebida_id IS NOT NULL;
+-- Inserir dados na tabela 'recheios'
+INSERT INTO recheios (recheio)
+VALUES 
+	('Bacon'),
+	('Queijo'),
+	('Frango'),
+	('Espinafre'),
+    ('Calabresa'),
+    ('Chocolate'),
+    ('Presunto'),
+    ('Ricota'),
+    ('Cogumelos'),
+    ('Tofu'),
+    ('Palmito'),
+    ('Abobrinha');
 
--- consulta 6: listar quais são os pastéis mais vendidos, incluindo a quantidade de vendas em ordem crescente
-SELECT P.nome, COUNT(IP.pastel_id) AS quantidade_vendas
-FROM Pasteis P
-JOIN ItensPedido IP ON P.pastel_id = IP.pastel_id
-GROUP BY P.nome
+    SELECT * FROM recheios;
+
+
+-- Inserir dados na tabela 'recheiosProdutos'
+INSERT INTO recheiosProdutos (produto_id, recheio_id)
+VALUES 
+	(1, 1),
+	(1, 2),
+	(2, 4),
+	(3, 2),
+	(3, 3),
+	(4, 3),
+	(8, 2),
+	(8, 3),
+	(9, 4),
+	(9, 5),
+	(10, 6),
+	(10, 7),
+	(11, 2),
+	(11, 3),
+	(12, 4),
+	(12, 9),
+	(13, 10),
+	(13, 11),
+	(14, 9),
+	(14, 10),
+	(15, 2),
+	(15, 3),
+	(15, 4),
+	(15, 9),
+	(15, 10);
+    
+    SELECT * FROM recheiosProdutos;
+
+
+-- Inserir dados na tabela 'pedidos'
+INSERT INTO pedidos (cliente_id, forma_pagamento)
+VALUES 
+    (1, 'PIX'),
+    (2, 'CC'),
+    (3, 'D'),
+    (1, 'CC'),
+    (2, 'PIX'),
+    (4, 'PIX'),
+    (5, 'D'),
+    (4, 'CC'),
+    (5, 'PIX'),
+    (1, 'D'),
+    (2, 'PIX'),
+    (3, 'CC'),
+    (1, 'PIX');
+    
+    SELECT * FROM pedidos;
+
+
+-- Inserir dados na tabela 'itensPedidos'
+INSERT INTO itensPedidos (pedido_id, produto_id, quantidade)
+VALUES 
+   (1, 1, 2),
+   (1, 2, 1),
+   (2, 3, 3),
+   (3, 4, 2),
+   (4, 1, 1),
+   (4, 5, 1),
+   (5, 2, 2),
+   (5, 5, 1),
+   (6, 8, 3),
+   (7, 9, 2),
+   (8, 10, 1),
+   (9, 8, 2),
+   (9, 9, 1),
+   (9, 10, 3),
+   (10, 11, 2),
+   (10, 12, 1),
+   (11, 13, 3),
+   (12, 14, 2),
+   (13, 15, 2);
+   
+    SELECT * FROM itensPedidos;
+    
+    
+-- Liste os nomes de todos os pastéis veganos vendidos para pessoas com mais de 18 anos
+
+SELECT DISTINCT p.nome
+FROM clientes c
+JOIN pedidos ped ON c.cliente_id = ped.cliente_id
+JOIN itensPedidos itp ON ped.pedido_id = itp.pedido_id
+JOIN produtos p ON itp.produto_id = p.produto_id
+JOIN categorias cat ON p.categoria_id = cat.categoria_id
+JOIN tamanhosProdutos tp ON p.produto_id = tp.produto_id
+JOIN tamanhos t ON tp.tamanho_id = t.tamanho_id
+WHERE c.data_nascimento < CURDATE() - INTERVAL 18 YEAR
+AND cat.nome = 'Vegano';
+  
+  
+  -- Liste os clientes com maior número de pedidos realizados em 1 ano agrupados por mês
+  
+SELECT
+CONCAT(MONTH(pe.data_pedido), '/', YEAR(pe.data_pedido)) AS mes_ano,
+c.nome_completo,
+COUNT(pe.pedido_id) AS numero_pedidos
+FROM pedidos pe
+JOIN clientes c ON pe.cliente_id = c.cliente_id
+WHERE pe.data_pedido >= CURDATE() - INTERVAL 1 YEAR
+GROUP BY mes_ano, c.nome_completo
+ORDER BY numero_pedidos DESC;
+
+
+-- Liste todos os pastéis que possuem bacon ou queijo em seu recheio
+
+SELECT DISTINCT p.nome
+FROM produtos p
+JOIN recheiosProdutos rp ON p.produto_id = rp.produto_id
+JOIN recheios r ON rp.recheio_id = r.recheio_id
+WHERE r.recheio IN ('Bacon', 'Queijo');
+
+
+-- Mostre o valor de venda total de todos os pastéis cadastrados no sistema
+
+SELECT SUM(tp.preco * ip.quantidade) AS valor_total
+FROM tamanhosProdutos tp
+JOIN itensPedidos ip ON tp.produto_id = ip.produto_id;
+
+-- Liste todos os pedidos onde há pelo menos um pastel e uma bebida
+
+SELECT DISTINCT ped.pedido_id, ped.data_pedido, ped.forma_pagamento
+FROM pedidos ped
+JOIN itensPedidos itp ON ped.pedido_id = itp.pedido_id
+JOIN produtos p ON itp.produto_id = p.produto_id
+JOIN categorias cat ON p.categoria_id = cat.categoria_id
+WHERE cat.nome IN ('Pasteis', 'Bebidas');
+
+
+-- Liste quais são os pastéis mais vendidos, incluindo a quantidade de vendas em ordem crescente
+
+SELECT p.nome, SUM(ip.quantidade) AS quantidade_vendas
+FROM produtos p
+JOIN itensPedidos ip ON p.produto_id = ip.produto_id
+GROUP BY p.nome
 ORDER BY quantidade_vendas ASC;
+
+
+
+
